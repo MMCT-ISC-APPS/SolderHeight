11a335160696e7e5feeb817eb5fe6b6ad7c7eb0e
ธีรภัทร เชื้อนุ่น <pudjaza0014@live.com>
2022-08-16 07:20:40 +0700

    .

--- View/frmSerialNo.Designer.cs
+++ View/frmSerialNo.Designer.cs
@@ -130,7 +130,7 @@ namespace SolidHeight.View
             this.txtJobName.Location = new System.Drawing.Point(123, 3);
             this.txtJobName.Name = "txtJobName";
             this.txtJobName.ReadOnly = true;
-            this.txtJobName.Size = new System.Drawing.Size(154, 30);
+            this.txtJobName.Size = new System.Drawing.Size(154, 26);
             this.txtJobName.TabIndex = 2;
             // 
             // txtTotalPCCA
@@ -143,7 +143,7 @@ namespace SolidHeight.View
             this.txtTotalPCCA.Location = new System.Drawing.Point(123, 35);
             this.txtTotalPCCA.Name = "txtTotalPCCA";
             this.txtTotalPCCA.ReadOnly = true;
-            this.txtTotalPCCA.Size = new System.Drawing.Size(154, 30);
+            this.txtTotalPCCA.Size = new System.Drawing.Size(154, 26);
             this.txtTotalPCCA.TabIndex = 3;
             // 
             // dgvSerialList
@@ -249,14 +249,13 @@ namespace SolidHeight.View
             // 
             // frmSerialNo
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
-            this.ClientSize = new System.Drawing.Size(571, 1018);
+            this.ClientSize = new System.Drawing.Size(430, 823);
             this.Controls.Add(this.tblfrmSerialList);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmSerialNo";
             // 
             // 
